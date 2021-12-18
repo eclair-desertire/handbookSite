b97a9acc4409e4956beceb041716e7f861293455
Alisher Ruziyev <eclair.desertire@gmail.com>
2021-12-18 23:55:24 +0600

    added main urls, category pages

--- handbook/urls.py
+++ handbook/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('catalog/',views.catalog,name='catalog'),
+    path('catalog/ishop',views.ishop,name="ishop"),
+    path('catalog/smarket',views.smarket,name='smarket'),
+    path('catalog/shopping',views.shopping,name='shopping'),
+    path('catalog/fshop',views.fshop,name='fshop'),
+    path('catalog/icompany',views.icompany,name='icompany'),
+    path('catalog/edu',views.edu,name='edu'),
 ]
--- handbook/views.py
+++ handbook/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render
 from .models import *
 
 def main_page(request):
-    companies=Company.objects.all()
-    rates=Rate.objects.all()
-    return render(request,'handbook/main_page.html',{'companies':companies,'rates':rates})
+    return render(request,'handbook/main_page.html',{})
 
-def catalog(request):
-    return render(request,'handbook/catalog_of_companies.html',{})
+def ishop(request):
+    return render(request,'handbook/ishop.html',{})
+
+def smarket(request):
+    return render(request,'handbook/smarket.html',{})
+
+def shopping(request):
+    return render(request,'handbook/shopping.html',{})
+
+def fshop(request):
+    return render(request,'handbook/fshop.html',{})
+
+def icompany(request):
+    return render(request,'handbook/icompany.html',{})
+
+def edu(request):
+    return render(request,'handbook/edu.html',{})
 # Create your views here.
